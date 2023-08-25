a721c615c00e5938c91e2aa0c92b3867f17ce9f6
Sharath <sharathkota070@gmail.com>
2023-08-25 16:53:31 +0530

    Final Commit

---
+++ app/src/main/java/com/sharath070/frenchteacher/MainActivity.kt
@@ -0,0 +1,25 @@
+  package com.sharath070.frenchteacher
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+  class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sayColor(view: View){
+        val clickedBtn: Button = view as Button
+
+        var mediaPlayer: MediaPlayer = MediaPlayer.create(
+            this,
+            resources.getIdentifier(clickedBtn.tag.toString(), "raw", packageName)
+        )
+        mediaPlayer.start()
+    }
+
+}
